6b615c91526870bc0caabd19d4eb85763eea9e2d
MinhDuy27 <146503855+MinhDuy27@users.noreply.github.com>
2023-09-30 13:18:32 +0700

    Add files via upload

---
+++ scripts/API/API/middlewares/auth.js
@@ -0,0 +1,10 @@
+const { validatesignature } = require('../../Database/side-function/side1');
+
+module.exports = async (req,res,next) => {
+    
+    const isauthorized = await validatesignature(req);
+    if(isauthorized){
+        return next();
+    }
+    return res.status(403).json({message: 'not authorized'})
+}
