0f833d54ad8c818503f34cb654586256ed8712da
MinhDuy27 <146503855+MinhDuy27@users.noreply.github.com>
2023-09-30 13:37:04 +0700

    Add files via upload

---
+++ service/product-service.js
@@ -0,0 +1,83 @@
+const { productrepository } = require('../Database');
+const { formatedata } = require('../Database/side-function/side1');
+//const { APIError } = require('../utils/app-errors');
+
+// All Business logic will be here
+class productservice {
+
+    constructor(){
+        this.repository = new productrepository();
+    }
+
+    async createproduct(productinputs){
+        try{
+            const productresult = await this.repository.createproduct(productinputs)
+            return formatedata(productresult);
+        }catch(err){
+            throw err;
+        }
+    }
+    async deleteproductbyid(productid){
+        try{
+            const result = this.repository.deleteproductbyid(productid)
+            return formatedata(result)
+        }
+        catch(err){
+            throw err
+        } 
+    }
+    
+
+    async getproducts(){
+        try{
+            const products = await this.repository.getproducts();
+            let categories = {};
+            products.map(({ type }) => {
+                categories[type] = type;
+            });
+            return formatedata(products)
+        }catch(err){
+            throw err;
+        }
+    }
+
+    async getproductsbycategory(category){
+        try {
+            const products = await this.repository.findbycategory(category);
+            return formatedata(products)
+        } catch (err) {
+            throw err;
+        }
+
+    }
+
+    async getselectedproducts(selectedIds){
+        try {
+            const products = await this.repository.findselectedproducts(selectedIds);
+            return formatedata(products);
+        } catch (err) {
+            throw err;
+        }
+    }
+
+    async getproductbyid(productId){
+        try {
+            const product = await this.repository.findbyid(productId);
+            return formatedata(product);
+        } catch (err) {
+            throw err;
+        }
+    }
+    
+    async getproductinpriceorder(sortorder,category){ // 1  ascending , -1 descending
+        try {
+            const product = await this.repository.findproductsbyprice(sortorder,category);  
+            return formatedata(product);    
+        } 
+        catch (err) {
+            throw err;
+        }
+    }
+}
+
+module.exports = productservice;
---
+++ service/users-service.js
@@ -0,0 +1,97 @@
+const { userrepository } = require("../Database");
+const {  generatepassword, formatedata, generatesignature, validatepassword,generatesalt } = require('../Database/side-function/side1');
+//const { APIError, BadRequestError } = require('../utils/app-errors')
+
+
+//logic
+class usersservice {
+    constructor(){
+        this.repository = new userrepository();
+    }
+    async login(userinputs){
+
+        const { email, password } = userinputs;
+        
+        try { 
+            const existingusers = await this.repository.findusers({email});
+            if(existingusers){
+            
+                const validPassword = await validatepassword(password, existingusers.password, existingusers.salt);
+                
+                if(validPassword){
+                    const token = await generatesignature({ email: existingusers.email, _id: existingusers._id});
+                    return formatedata({id: existingusers._id, token });
+                } 
+            }
+            return formatedata(null);
+        } catch (err) {
+            throw err;
+        }
+    }
+    async signup(userinputs){
+
+        const { email, password, name,phone } = userinputs;
+        const existingusers = await this.repository.findusers({email});
+        if(existingusers)
+             return null;
+        else{
+            try{
+                // create salt
+                let salt = await generatesalt();
+                
+                let userPassword = await generatepassword(password, salt);
+                
+                const existinguser = await this.repository.createusers({ email, password: userPassword,name,salt,phone});
+                
+               // const token = await generatesignature({ email: email, _id: existinguser._id});
+    
+                return formatedata({id: existinguser._id });
+    
+            }catch(err){
+                throw err;
+            }
+        }    
+    }
+
+    async addnewaddress(_id,userinputs){
+        const { country, province, city,street} = userinputs;   
+        try {
+            const addressResult = await this.repository.createaddress({ _id,country, province, city, street})
+            return formatedata(addressResult);
+            
+        } catch (err) {
+            throw err;
+        }
+        
+    
+    }
+
+    async getprofile(id){
+        try {
+            const existinguser = await this.repository.findusersbyid({id});
+            return formatedata(existinguser);
+        } catch (err) {
+            throw err
+        }
+    }
+
+    async addtocart(userid, productid, qty, isRemove){
+        try {
+            const cartResult = await this.repository.addcartitem(userid, productid, qty, isRemove);        
+            return formatedata(cartResult);
+        } catch (err) {
+            throw err;
+        }
+    }
+
+    async addorder(userid, order){
+        try {
+            const orderResult = await this.repository.addordertoprofile(userid, order);
+            return formatedata(orderResult);
+        } catch (err) {
+            throw err;
+        }
+    }
+}
+
+module.exports = usersservice;
