d09064d2da09664e67c696dcf60509057cec44c5
MinhDuy27 <146503855+MinhDuy27@users.noreply.github.com>
2023-09-30 13:35:46 +0700

    Delete scripts/API directory

--- scripts/API/Database/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    productrepository: require('./functions/product-function'),
-    userrepository: require('./functions/user-fuction'),
-    shoppingrepository: require('./functions/shopping-function')
-}
