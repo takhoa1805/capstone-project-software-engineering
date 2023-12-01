8b776d88fd1047dbb80d6849ce092916a13ebda8
takhoa1805 <khoa.tran1805@hcmut.edu.vn>
2023-12-01 15:46:21 +0700

    working on selfs all orders

--- client/scripts/auth.js
+++ client/scripts/auth.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded',()=>{
     const isLogged = localStorage.getItem('user');
     if (!isLogged){
         window.alert("Please log in to continue");
+        window.location.href='/client/index.php';
     }
 });
 
